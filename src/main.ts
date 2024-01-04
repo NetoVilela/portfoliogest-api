5c44557e04607c4f6321962eded9c13151e384b1
NetoVilela <favneto.a.v.n@gmail.com>
2024-01-04 14:14:24 -0300

    feat: add certificates

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../certficates/private.key'),
+    cert: fs.readFileSync('../certficates/origin.crt'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
